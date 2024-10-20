1d2824acc8845792fe8cb589b59ff8fb2e7b8f8c
Le Phan Tri Duc <87744086+lephantriduc@users.noreply.github.com>
2024-10-20 23:15:34 +0700

    Add modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,11 @@ module org.example.uet_library {
     requires javafx.fxml;
     requires okhttp3;
     requires java.sql;
+    requires java.desktop;
+    requires io.github.cdimascio.dotenv.java;
+    requires bcrypt;
 
     opens org.example.uet_library to javafx.fxml;
     exports org.example.uet_library;
+    exports org.example.uet_library.Controllers;
 }
