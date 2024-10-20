c4f309c0225903dc8bfb4e6655f1ab459c12d6f9
Le Phan Tri Duc <87744086+lephantriduc@users.noreply.github.com>
2024-10-20 23:16:03 +0700

    Add entry point

--- src/main/java/org/example/uet_library/App.java
+++ src/main/java/org/example/uet_library/App.java
@@ -1,5 +1,18 @@
 package org.example.uet_library;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/LogIn.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
