2e5a3a8dbc77bbee386ca00553ec11ff650c19e0
Le Phan Tri Duc <87744086+lephantriduc@users.noreply.github.com>
2024-11-23 15:45:23 +0700

    Change Alert style

--- src/main/java/org/example/uet_library/AlertHelper.java
+++ src/main/java/org/example/uet_library/AlertHelper.java
@@ -1,16 +1,37 @@
 package org.example.uet_library;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 /**
  * For reusing Alerts. You guys (Viet and Hieu) can add more functions to suit your needs.
  */
 public class AlertHelper {
-    public static void showAlert(Alert.AlertType type, String title, String content) {
+    public static void showAlert(Alert.AlertType type, String boldText, String lightText) {
         Alert alert = new Alert(type);
-        alert.setTitle(title);
+        String header = switch (type) {
+            case INFORMATION -> "Info";
+            case WARNING -> "WARNING";
+            case ERROR -> "ERROR";
+            default -> "";
+        };
+        alert.setTitle(header);
         alert.setHeaderText(null);
-        alert.setContentText(content);
+
+        Label boldLabel = new Label(boldText);
+        boldLabel.setStyle("-fx-font-weight: bold; -fx-font-size: 15px;");
+
+        Label lightLabel = new Label(lightText);
+        lightLabel.setStyle("-fx-font-weight: normal; -fx-font-size: 13px;");
+
+        VBox content = new VBox(5, boldLabel, lightLabel);
+        content.setAlignment(Pos.CENTER_LEFT);
+
+        alert.getDialogPane().setContent(content);
+
         alert.showAndWait();
     }
+
 }
