db4ff894b488d6e2a330599a952fb57e225b194d
Le Phan Tri Duc <87744086+lephantriduc@users.noreply.github.com>
2024-11-25 11:44:06 +0700

    Rename for consistency

--- src/main/java/org/example/uet_library/models/Admin.java
+++ src/main/java/org/example/uet_library/models/Admin.java
@@ -2,9 +2,9 @@ package org.example.uet_library.models;
 
 public class Admin extends Account {
 
-    public Admin(int id, String username, String password, String firstName, String last_name,
+    public Admin(int id, String username, String password, String firstName, String lastName,
         String email) {
-        super(id, username, password, firstName, last_name, email);
+        super(id, username, password, firstName, lastName, email);
     }
 
     public Admin(String username, String firstName, String lastName,
